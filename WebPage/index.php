f34f032cc1294937ea4c6a21a9274ea1d105c7d0
Claudio <claudio.capobianco@marconicloud.it>
2021-03-03 09:31:16 +0100

    Add front-end with HTML
    Added index.html, script.js and JSON exchange

--- WebPage/index.php
+++ WebPage/index.php
@@ -1,39 +1,7 @@
 <?php
-   ob_start();
-   session_start();
+     $json = new stdClass();
+	 $json->username = "Mario Verdi";
+	 $json->poi_left_number = 2;
+
+	 echo json_encode($json);
 ?>
-
-<?
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);
-?>
-
-
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Index Cremona</title>
-		 <link href="styleIndex.css" rel="stylesheet" type="text/css" />
-	</head>
-	<body>
-	<?php
-		if ($_SESSION['valid'] != true)
-		{
-				header('Refresh: 0; URL = login.php');
-		}
-	?>
-	<p>Benvenuto <?php echo $_SESSION['username'] ?></p>
-	<p>Pacchetto <?php echo $_SESSION['subscription'] ?></p>
-	<p class = "p_center"> POI rimanenti: <?php echo $_SESSION['poileft'] ?> </p>
-		<ul>
- 		  <li>Museo Del Violino</li>
-  		<li>Museo della Civilta' Contadina</li>
-  		<li>Museo Civico</li>
-  		<li>Loggia dei Militi</li>
-		</ul>
-	<p class = "p_center"><button class = "navbtn" type = "submit" name = "navigation">NAVIGATION</button></p>
-	<p><a href = "logout.php" tite = "Logout">Logout.</p>
-	</body>
-</html>
