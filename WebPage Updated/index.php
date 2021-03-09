9c7d9db5ae4685019277a9fe850af60416ba75bf
ObvTella <72648253+ObvTella@users.noreply.github.com>
2021-03-09 10:34:16 +0100

    Delete Webpage Update
    project clean up

--- WebPage Updated/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $json = new stdClass();
-	 $json->username = "Mario Verdi";
-	 $json->poi_left_number = 6;
-	 $json->subscription = "Argento";
-	 $json->poi_list = array("<li>Museo Del Violino</li>","<li>Loggia dei Militi</li>","<li>Museo Archeologico</li>", "<li>Museo della Civiltà Contadina</li>", "<li>Museo Verticale</li>", "<li>Museo Civico</li>");
-	 echo json_encode($json);
-?>
--- WebPage Updated/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-   session_start();
-   unset($_SESSION["username"]);
-   unset($_SESSION["password"]);
-   
-   echo 'Stai sloggando';
-   header('Refresh: 0; URL = login.php');
-?>
